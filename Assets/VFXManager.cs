7a7a4eb07fd8ec814da102a30afaafe65c70e3a7
Ravi Bhatt <rnbhatt@umich.edu>
2023-02-05 12:26:13 -0500

    Fix knockback smoke

--- Assets/VFXManager.cs
+++ Assets/VFXManager.cs
@@ -7,6 +7,9 @@ public class VFXManager : MonoBehaviour
 {
 
     public VisualEffect Instance;
+    public float destroyAfter = 10.0f;
+    public bool stopEarly = false;
+    public float stopEarlyAfter = 3.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -14,17 +17,21 @@ public class VFXManager : MonoBehaviour
         Instance.Play();
 
         StartCoroutine(Destroytimer());
+        if (stopEarly)
+        {
+            StartCoroutine(StopTimer());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator StopTimer()
     {
-        
+        yield return new WaitForSeconds(stopEarlyAfter);
+        Instance.Stop();
     }
 
     IEnumerator Destroytimer()
     {
-        yield return new WaitForSeconds(10);
+        yield return new WaitForSeconds(destroyAfter);
         Destroy(Instance);
     }
     
