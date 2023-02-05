f32fe1ffbe2a9179e01a6f5666f60efa0076ecb2
jazoland26 <jazoland26@gmail.com>
2023-02-04 23:55:57 -0500

    player inventory
    ui

--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -1,10 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerUI : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public PlayerController player;
+    public int item1Capacity;
+    public int item2Capacity;
+    public TMP_Text item1Text;
+    public TMP_Text item2Text;
+
+    int acornCapacity = 5;
+    int seedCapacity = 10;
+    int meleeCapacity = 15;
+    int shieldCapacity = 10;
+
+
     void Start()
     {
         
@@ -13,6 +26,19 @@ public class PlayerUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (player.item1)
+        {
+            item1Capacity = player.item1.capacity;
+            item1Text.text = item1Capacity.ToString();
+            
+        }
+
+        if (player.item2)
+        {
+            item2Capacity = player.item2.capacity;
+            item2Text.text = item2Capacity.ToString();
+        }
+
         
     }
 }
