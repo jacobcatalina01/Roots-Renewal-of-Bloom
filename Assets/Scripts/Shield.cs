1b5b5845151b67478958b91b3c6fee48b5baa176
dqanxy-umich <83249976+dqanxy@users.noreply.github.com>
2023-02-05 00:07:02 -0500

    E

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class Shield : MonoBehaviour
 {
-
+    [SerializeField] PlayerController pc;
     private void OnTriggerEnter(Collider other)
     {
 
-        if (other.gameObject.CompareTag("Bullet"))
+        if (other.gameObject.CompareTag("Bullet") && !other.GetComponent<GameObjectRef>().go.Contains(pc.gameObject))
+
         {
             Destroy(other.gameObject);
         }
