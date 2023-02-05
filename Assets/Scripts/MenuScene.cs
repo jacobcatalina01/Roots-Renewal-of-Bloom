0e5e2fe52d7e3ac5fc33e47d8ca6c0e8ca89eb15
Isaac Huffman <isaacphuffman@gmail.com>
2023-02-05 16:46:05 -0500

    Add credits

--- Assets/Scripts/MenuScene.cs
+++ Assets/Scripts/MenuScene.cs
@@ -6,17 +6,32 @@ public class MenuScene : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject startScreen;
+    public GameObject creditsScreen;
+
+    public void Start()
+    {
+        GoToMainMenu();
+    }
 
     public void GoToMainMenu()
     {
         mainMenu.SetActive(true);
         startScreen.SetActive(false);
+        creditsScreen.SetActive(false);
     }
 
     public void GoToStartScreen()
     {
         mainMenu.SetActive(false);
         startScreen.SetActive(true);
+        creditsScreen.SetActive(false);
+    }
+
+    public void GoToCredits()
+    {
+        mainMenu.SetActive(false);
+        startScreen.SetActive(false);
+        creditsScreen.SetActive(true);
     }
 
     public void QuitGame()
